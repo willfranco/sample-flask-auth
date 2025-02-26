b9b1927c7350b324d2fbda644b42727a2ed4ead6
William Franco <william_afranco@hotmail.com>
2025-02-25 22:59:53 -0300

    Projeto autenticação em API

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 
 app = Flask(__name__)
@@ -19,85 +20,88 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(user_id)
 
-
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=["POST"])
 def login():
     data = request.json
-    username = data.get('username')
-    password = data.get('password')
+    username = data.get("username")
+    password = data.get("password")
 
     if username and password:
+        # Login
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             print(current_user.is_authenticated)
-            return jsonify({'message': 'Autenticação realizada com sucesso '})
+            return jsonify({"message": "Autenticação realizada com sucesso"})
 
-    return jsonify({'message': 'Credenciais inválidas'}), 400
-
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 @app.route('/logout', methods=['GET'])
 @login_required
 def logout():
     logout_user()
-    return jsonify({'message': 'Logout realizado com sucesso!'})
+    return jsonify({"message": "Logout realizado com sucesso!"})
 
-
-@app.route('/user', methods=['POST'])
+@app.route('/user', methods=["POST"])
 def create_user():
     data = request.json
-    username = data.get('username')
-    password = data.get('password')
+    username = data.get("username")
+    password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password)
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role='user')
         db.session.add(user)
         db.session.commit()
-        return jsonify({'message': 'Usuário cadastrado com sucesso'})
+        return jsonify({"message": "Usuario cadastrado com sucesso"})
 
-    return jsonify({'message': 'Dados inválidos'}), 400
+    return jsonify({"message": "Dados invalidos"}), 400
 
-
-@app.route('/user/<int:id_user>', methods=['GET'])
+@app.route('/user/<int:id_user>', methods=["GET"])
 @login_required
 def read_user(id_user):
     user = User.query.get(id_user)
 
     if user:
-        return {'username': user.username}
-    
-    return jsonify({'message': 'Usuário não encontrado'}), 404
+        return {"username": user.username}
 
+    return jsonify({"message": "Usuario não encontrado"}), 404
 
-@app.route('/user/<int:id_user>', methods=['PUT'])
+@app.route('/user/<int:id_user>', methods=["PUT"])
 @login_required
 def update_user(id_user):
     data = request.json
     user = User.query.get(id_user)
 
-    if user and data.get('password'):
-        user.password = data.get('password')
+    if id_user != current_user.id and current_user.role == "user":
+        return jsonify({"message": "Operação não permitida"}), 403
+
+    if user and data.get("password"):
+        user.password = data.get("password")
         db.session.commit()
 
-        return jsonify({'message': f'Usuário {id_user} atualizado com sucesso!'})
+        return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
+    
+    return jsonify({"message": "Usuario não encontrado"}), 404
 
-    return jsonify({'message': 'Usuário não encontrado'}), 404
-
-
-@app.route('/user/<int:id_user>', methods=['DELETE'])
+@app.route('/user/<int:id_user>', methods=["DELETE"])
 @login_required
 def delete_user(id_user):
     user = User.query.get(id_user)
+    
+    if current_user.role != 'admin':
+        return jsonify({"message": "Operação não permitida"}), 403
 
-    if user and id_user != current_user.id:
+    if id_user == current_user.id:
+        return jsonify({"message": "Deleção não permitida"}), 403
+
+    if user:
         db.session.delete(user)
         db.session.commit()
-        return jsonify({'message': f'Usuário {id_user} deletado com sucesso!'})
-
-    return jsonify({'message': 'Usuário não encontrado'}), 404
-
-
+        return jsonify({"message": f"Usuário {id_user} deletado com sucesso"})
+    
+    return jsonify({"message": "Usuario não encontrado"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
